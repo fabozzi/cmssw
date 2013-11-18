93a77f33e57c1b16409bdb4c90b609400bad9584
Francesco <Francesco.Fabozzi@cern.ch>
2013-11-18 19:20:54 +0100

    fixing prototype for EI validation sequence

--- DQM/Physics/python/DQMPhysicsEI_cff.py
+++ DQM/Physics/python/DQMPhysicsEI_cff.py
@@ -13,12 +13,16 @@ pfMETValidationEI.InputCollection = cms.InputTag('pfMetEI')
 pfMETValidationEI.MatchCollection = cms.InputTag('pfMet')
 pfMETValidationEI.BenchmarkLabel  = cms.string('PFMETValidationEI/CompWithPFMet')
 
+pfAllElectrons = cms.EDFilter("PdgIdPFCandidateSelector",
+                              pdgId = cms.vint32(11, -11),
+                              src = cms.InputTag("particleFlow")
+)
+
 from DQMOffline.PFTau.PFElectronDQMAnalyzer_cfi import pfElectronDQMAnalyzer
 pfElectronValidationEI = pfElectronDQMAnalyzer.clone()
 pfElectronValidationEI.InputCollection = cms.InputTag('pfIsolatedElectronsEI')
 pfElectronValidationEI.MatchCollection = cms.InputTag('pfAllElectrons')
-pfElectronValidationEI.BenchmarkLabel  = cms.string('PFElectronValidationEI/CompWithPFElectron')
-
+pfElectronValidationEI.BenchmarkLabel  = cms.string('PFElectronValidationEI/CompWithGsfElectron')
 
 from DQM.Physics.susyDQM_cfi import *
 susyDQM_EI = susyDQM.clone()
@@ -29,15 +33,28 @@ susyDQM_EI.jetCollection = cms.InputTag('pfJetsEI')
 susyDQM_EI.metCollection = cms.InputTag('pfMetEI')
 
 
-topDiLeptonOfflineDQM_EI = cms.EDAnalyzer("TopDiLeptonOfflineDQM",
+singleTopDQM_EI = cms.EDAnalyzer("SingleTopTChannelLeptonDQM",
+  ## ------------------------------------------------------
+  ## SETUP
+  ##
+  ## configuration of the MonitoringEnsemble(s)
+  ## [mandatory] : optional PSets may be omitted
+  ##
   setup = cms.PSet(
-    directory = cms.string("Physics/TopEI/TopDiLeptonDQM/"),
+    ## sub-directory to write the monitor histograms to
+    ## [mandatory] : should not be changed w/o explicit 
+    ## communication to TopCom!
+    directory = cms.string("Physics/TopEI/SingleTopDQM/"),
+    ## [mandatory]
     sources = cms.PSet(
       muons = cms.InputTag("pfIsolatedMuonsEI"),
+      elecs_gsf = cms.InputTag("gsfElectrons"),
       elecs = cms.InputTag("pfIsolatedElectronsEI"),
       jets  = cms.InputTag("pfJetsEI"),
-      mets  = cms.VInputTag("met", "tcMet", "pfMetEI")
-      ),
+      mets  = cms.VInputTag("met", "tcMet", "pfMetEI"),
+      pvs   = cms.InputTag("offlinePrimaryVertices")
+    ),
+    ## [optional] : when omitted the verbosity level is set to STANDARD
     monitoring = cms.PSet(
       verbosity = cms.string("DEBUG")
     ),
@@ -54,7 +71,8 @@ topDiLeptonOfflineDQM_EI = cms.EDAnalyzer("TopDiLeptonOfflineDQM",
 
 dqmPhysicsEI = cms.Sequence( pfJetValidationEI *
                              pfMETValidationEI *
+                             pfAllElectrons *
                              pfElectronValidationEI *
                              susyDQM_EI *
-                             topDiLeptonOfflineDQM_EI )
+                             singleTopDQM_EI )
 
